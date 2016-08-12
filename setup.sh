67732ce41e78753166d69eb50254059c8ab9bc6c
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-08-12 17:18:06 +0100

    Updated setup.sh to be a rough draft of a proper script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+usage() {
+	echo "$0 [dotfiles|mutt|packages|aur_packages|wallpapers|pacaur] [-a]"
+	echo "    dotfiles - Create symlinks for all dotfiles and exit"
+	echo "    mutt - Setup mutt"
+	echo "    packages - Install all required packages and exit"
+	echo "    aur_packages - Install cower and pacaur (if required), then install all required AUR packages and exit"
+	echo "    wallpapers - Install wallpapers to ~/.wallpapers"
+	echo "    pacaur - Install pacaur (pre-requisite for aur_packages)"
+	echo ""
+	echo " -a Ask for confirmation before changing anything. I.e. before each dotfile is linked, before any packages are downloaded etc."
+	echo " -h Print this help text"
+}
+
 is_installed() {
 	if [[ -z $(pacman -Qs ^$1$) ]]; then
 		return 1
@@ -7,62 +20,6 @@ is_installed() {
 		return 0
 	fi
 }
-
-FILES=( .zshrc .xinitrc .Xmodmap .gitconfig .gitignore_global .vimrc .gradle .muttrc .tmux.conf .vnc/xstartup .config/i3/config .config/neofetch/config .newsbeuter/urls .xscreensaver )
-PACKAGES=( git tmux i3 xscreensaver newsbeuter terminator scrot feh )
-AUR_PACKAGES=( neofetch )
-DOTFILES=$(pwd)
-
-echo "Setting up symlinks for dotfiles..."
-for f in ${FILES[@]}; do
-	if [ ! -e ~/$f ]; then
-		echo Creating symlink for $f
-
-		# Create the parent directory if it doesn't exist
-		mkdir -p $(dirname ~/$f)
-		ln -s $DOTFILES/$f ~/$f
-	else
-		echo "~/$f already exists, skipping..."
-	fi
-done
-
-mkdir -p ~/.mutt/tmp
-
-if [ ! -f ~/.mutt/imapconfig ]; then
-	cp $DOTFILES/exampleimapconfig ~/.mutt/imapconfig
-	echo "Don't forget to update ~/.mutt/imapconfig with your email settings!"
-fi
-
-echo ""
-echo "Attempting to install required packages:"
-
-TO_INSTALL=()
-
-for p in ${PACKAGES[@]}; do
-	if is_installed $p; then
-		echo "$p already installed"
-	else
-		echo $p
-		TO_INSTALL+=($p)
-	fi
-done
-
-if [ ${#TO_INSTALL[@]} -gt 0 ]; then
-	sudo pacman -S ${TO_INSTALL[*]}
-fi
-
-echo "Installing pacaur"
-
-CURRENT_DIR=`pwd`
-
-mkdir -p /tmp/pacaur_install
-cd /tmp/pacaur_install
-
-
-# Git is required for aur installs
-if ! is_installed git; then
-	sudo pacman -S git
-fi
 
 install_from_aur() {
 	# Check it's already installed
@@ -75,21 +32,152 @@ install_from_aur() {
 	fi
 }
 
-# Install cower
-install_from_aur cower
+confirm() {
+	if [[ 0 -eq $ASK_CONFIRM ]]; then
+		return 0
+	fi
 
-# Install pacaur
-install_from_aur pacaur
+	read -p "$1 (y/n)" -n 1 -r
+	echo
 
-cd $CURRENT_DIR
-rm -r /tmp/pacaur_install
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		return 0
+	fi
 
-echo "Pacaur installation complete, installing AUR packages..."
+	return 1
 
-pacaur -S ${AUR_PACKAGES[*]}
+}
 
-echo "Setting up wallpapers etc"
-mkdir -p ~/.wallpapers
-cp $DOTFILES/images/wallpapers/* ~/.wallpapers/
+ASK_CONFIRM=0
+FILES=( .zshrc .xinitrc .Xmodmap .gitconfig .gitignore_global .vimrc .gradle .muttrc .tmux.conf .vnc/xstartup .config/i3/config .config/neofetch/config .newsbeuter/urls .xscreensaver )
+PACKAGES=( git tmux i3 xscreensaver newsbeuter terminator scrot feh )
+AUR_PACKAGES=( neofetch )
+DOTFILES=$(pwd)
+
+symlink_dotfiles() {
+	echo "Setting up symlinks for dotfiles..."
+	for f in ${FILES[@]}; do
+		if [[ ! -e ~/$f ]]; then
+			if confirm "Symlink $DOTFILES/$f to ~f?"; then
+
+				echo Creating symlink for $f
+
+				# Create the parent directory if it doesn't exist
+				mkdir -p $(dirname ~/$f)
+				ln -s $DOTFILES/$f ~/$f
+			fi
+		else
+			echo "~/$f already exists, skipping..."
+		fi
+	done
+}
+
+setup_mutt() {
+	mkdir -p ~/.mutt/tmp
+
+	if [ ! -f ~/.mutt/imapconfig ]; then
+		cp $DOTFILES/exampleimapconfig ~/.mutt/imapconfig
+		echo "Don't forget to update ~/.mutt/imapconfig with your email settings!"
+	fi
+}
+
+install_packages() {
+	echo ""
+	echo "Attempting to install required packages:"
+
+	TO_INSTALL=()
+
+	for p in ${PACKAGES[@]}; do
+		if is_installed $p; then
+			echo "$p already installed"
+		else
+			echo $p
+			TO_INSTALL+=($p)
+		fi
+	done
+
+	if [ ${#TO_INSTALL[@]} -gt 0 ]; then
+		sudo pacman -S ${TO_INSTALL[*]}
+	fi
+}
+
+install_pacaur() {
+	echo "Installing pacaur"
+
+	CURRENT_DIR=`pwd`
+
+	mkdir -p /tmp/pacaur_install
+	cd /tmp/pacaur_install
+
+
+	# Git is required for aur installs
+	if ! is_installed git; then
+		sudo pacman -S git
+	fi
+
+
+	# Install cower
+	install_from_aur cower
+
+	# Install pacaur
+	install_from_aur pacaur
+
+	cd $CURRENT_DIR
+	rm -r /tmp/pacaur_install
+
+	echo "Completed installing pacaur"
+}
+
+install_aur_packages() {
+	pacaur -S ${AUR_PACKAGES[*]}
+}
+
+setup_wallpapers() {
+	echo "Setting up wallpapers etc"
+	mkdir -p ~/.wallpapers
+	cp $DOTFILES/images/wallpapers/* ~/.wallpapers/
+}
+
+while getopts ":ah" opt; do
+	case $opt in
+		a)
+			ASK_CONFIRM=1
+			;;
+		h)
+			usage
+			exit;;
+		\?)
+			echo "Invalid option : -$OPTARG"
+			;;
+	esac
+done
+
+shift $(($OPTIND -1))
+ARGUMENTS=$*
+
+[[ -z $ARGUMENTS ]] && ARGUMENTS="dotfiles mutt packages aur_packages wallpapers pacaur"
+
+DO_DOTFILES=1
+DO_MUTT=1
+DO_PACKAGES=1
+DO_AUR_PACKAGES=1
+DO_SETUP_WALLPAPAERS=1
+DO_INSTALL_PACAUR=1
+
+for arg in $ARGUMENTS; do
+	[[ $arg == "dotfiles" ]] && DO_DOTFILES=0
+	[[ $arg == "mutt" ]] && DO_MUTT=0
+	[[ $arg == "packages" ]] && DO_PACKAGES=0
+	[[ $arg == "aur_packages" ]] && DO_AUR_PACKAGES=0
+	[[ $arg == "wallpapers" ]] && DO_SETUP_WALLPAPERS=0
+	[[ $arg == "pacaur" ]] && DO_INSTALL_PACAUR=0
+done
+
+[[ $DO_DOTFILES == 0 ]] && symlink_dotfiles
+[[ $DO_MUTT == 0 ]] && setup_mutt
+[[ $DO_PACKAGES == 0 ]] && install_packages
+[[ $DO_INSTALL_PACAUR == 0 ]] && install_pacaur
+[[ $DO_AUR_PACKAGES == 0 ]] && install_aur_packages
+[[ $DO_SETUP_WALLPAPERS == 0 ]] && setup_wallpapers
 
 echo Done!
