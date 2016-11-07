c8e2f454327ff95af149eda49284bc8c5f15bb41
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-11-07 10:00:04 +0000

    Removed titlebar/borders from i3 and added lock function

---
+++ .config/i3/lock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scrot /tmp/screenshot.png
+ffmpeg -loglevel quiet -y -i /tmp/screenshot.png -vf "gblur=sigma=20" /tmp/screenshot_blur.png
+i3lock -u -i /tmp/screenshot_blur.png
+rm /tmp/screenshot.png /tmp/screenshot_blur.png
