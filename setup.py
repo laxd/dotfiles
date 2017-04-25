bf372e92feb9562779b5b1c34e64ff8a53b979dc
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2017-04-25 09:59:39 +0100

    Added basic logging

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ import argparse
 import glob
 import socket
 import os
+import logging
 from subprocess import call
 
 def merge_files(output, patterns=[], comment="#"):
     filenames = []
     for pattern in patterns:
         filenames.extend(glob.glob(pattern))
+
+    logging.debug("Found {} from patterns: {}".format(", ".join(filenames), ", ".join(patterns)))
 
     open(output, 'w').close()
     config = open(output, 'a')
@@ -22,9 +25,12 @@ def merge_files(output, patterns=[], comment="#"):
         file.close()
 
     config.close()
+    logging.debug("Finished writing {}".format(output))
 
 def is_installed(package):
-    return not call(["pacman", "-Qs", "^{}$".format(package)])
+    is_installed = not call(["pacman", "-Qs", "^{}$".format(package)])
+    logging.debug("{} installed? {}".format(package, is_installed))
+    return is_installed
 
 packages=["git", "binutils", "gcc", "make", "fakeroot", "tmux", "i3", "xscreensaver", "newsbeuter", "rxvt-unicode", "urxvt-perls", "scrot", "feh", "base-devel", "expac", "sysstat", "imagemagick", "xautolock", "dex", "zsh"]
 dotfiles=[".zshrc", ".xinitrc", ".xprofile", ".gitconfig", ".gitignore_global", ".vimrc", ".gradle", ".muttrc", ".tmux.conf", ".vnc/xstartup", ".config/i3/config", ".config/i3/lock.sh", ".config/i3/lock.png", ".config/i3status/config", ".config/neofetch/config", ".newsbeuter/urls", ".xscreensaver", ".Xdefaults", ".config/fontconfig/fonts.conf"]
@@ -32,30 +38,37 @@ dotfiles=[".zshrc", ".xinitrc", ".xprofile", ".gitconfig", ".gitignore_global", 
 parser = argparse.ArgumentParser()
 parser.add_argument("-A", "--all", help="Perform a full setup including ALL options", action="store_true")
 parser.add_argument("-a", "--aur", help="Install AUR packages", action="store_true")
-parser.add_argument("-c", "--confirm", help="Confirm actions", action="store_true")
+# parser.add_argument("-c", "--confirm", help="Confirm actions", action="store_true")
 parser.add_argument("-d", "--dotfiles", help="Symlink dotfiles", action="store_true")
-parser.add_argument("-f", "--force", help="Overwrite files, even if they exist", action="store_true")
+# parser.add_argument("-f", "--force", help="Overwrite files, even if they exist", action="store_true")
 parser.add_argument("-P", "--install-pacaur", help="Install pacaur", action="store_true")
 parser.add_argument("-i", "--configure-i3", help="Configure i3", action="store_true")
 parser.add_argument("-m", "--configure-mutt", help="Configure Mutt", action="store_true")
 parser.add_argument("-p", "--install-packages", help="Install packages", action="store_true")
-parser.add_argument("-v", "--verbose", help="Increase logging", action="store_true")
+parser.add_argument("-v", "--verbose", help="Increase logging", action="count", default=0)
 parser.add_argument("-w", "--wallpapers", help="Copy wallpapers", action="store_true")
 args = parser.parse_args()
 
-if args.configure_i3:
-    print("Configuring i3")
+if args.verbose == 0:
+    logging.basicConfig(level=logging.WARNING, format='%(message)s')
+elif args.verbose == 1:
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+elif args.verbose >= 2:
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+if args.configure_i3 or args.all:
+    logging.info("Configuring i3")
     merge_files(".config/i3/config", [".config/i3/*-all.config", ".config/i3/*-{}.config".format(socket.gethostname())])
 
-if args.dotfiles:
-    print("Symlinking dotfiles")
+if args.dotfiles or args.all:
+    logging.info("Symlinking dotfiles")
 
     for dotfile in dotfiles:
         # Create parent dir first
-        print(" ".join(["mkdir", "-p", "\"$(dirname $HOME/{})\"".format(dotfile)]))
-        print(" ".join(["ln", "-s", "{}/{}".format(os.getcwd(), dotfile), "$HOME/{}".format(dotfile)]))
+        call(["mkdir", "-p", "\"$(dirname $HOME/{})\"".format(dotfile)])
+        call(["ln", "-s", "{}/{}".format(os.getcwd(), dotfile), "$HOME/{}".format(dotfile)])
 
-if args.install_packages:
+if args.install_packages or args.all:
     print("Installing packages")
     install_targets = [package for package in packages if not is_installed(package)]
 
