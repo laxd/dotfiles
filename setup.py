0560279e9092c38236b9172a33847361d6adb1f8
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2017-04-29 12:00:52 +0100

    Implemented --force option for dotfiles

--- setup.py
+++ setup.py
@@ -46,15 +46,15 @@ aur_packages=["neofetch", "neomutt", "py3status", "urxvt-resize-font-git"]
 dotfiles=[".zshrc", ".xinitrc", ".xprofile", ".gitconfig", ".gitignore_global", ".vimrc", ".gradle", ".muttrc", ".tmux.conf", ".vnc/xstartup", ".config/i3/config", ".config/i3/lock.sh", ".config/i3/lock.png", ".config/i3status/config", ".config/neofetch/config", ".newsbeuter/urls", ".xscreensaver", ".Xdefaults", ".config/fontconfig/fonts.conf"]
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-A", "--all", help="Perform a full setup including ALL options", action="store_true")
+parser.add_argument("-A", "--all", help="Perform a full setup including ALL options. Equivalent to -dpPawi", action="store_true")
 # parser.add_argument("-c", "--confirm", help="Confirm actions", action="store_true")
 parser.add_argument("-d", "--dotfiles", help="Symlink dotfiles", action="store_true")
-# parser.add_argument("-f", "--force", help="Overwrite files, even if they exist", action="store_true")
+parser.add_argument("-f", "--force", help="Overwrite files, even if they exist", action="store_true")
 parser.add_argument("-P", "--install-pacaur", help="Install pacaur", action="store_true")
 parser.add_argument("-i", "--configure-i3", help="Configure i3", action="store_true")
 # parser.add_argument("-m", "--configure-mutt", help="Configure Mutt", action="store_true")
 parser.add_argument("-p", "--install-packages", help="Install packages", action="store_true")
-parser.add_argument("-a", "--install-aur-packages", help="Install AUR packages. If pacaur is not installed, adds --install-pacaur option", action="store_true")
+parser.add_argument("-a", "--install-aur-packages", help="Install AUR packages. If pacaur is not installed, implies --install-pacaur option", action="store_true")
 parser.add_argument("-v", "--verbose", help="Increase logging", action="count", default=0)
 parser.add_argument("-w", "--wallpapers", help="Copy wallpapers", action="store_true")
 args = parser.parse_args()
@@ -81,24 +81,41 @@ if args.dotfiles or args.all:
     logging.info("Symlinking dotfiles")
 
     for dotfile in dotfiles:
-        # Create parent dir first
-        call(["mkdir", "-p", "\"$(dirname $HOME/{})\"".format(dotfile)])
-        call(["ln", "-s", "{}/{}".format(os.getcwd(), dotfile), "$HOME/{}".format(dotfile)])
+        source="{}/{}".format(os.getcwd(), dotfile)
+        target="$HOME/{}".format(dotfile)
+
+        if not open(source, "r").exists:
+            logging.error("{} doesn't exist in repository".format(source))
+            continue
+
+        # Create parent dir first, just in case it doesn't exist.
+        target_dir=call(["dirname", target]).stdout
+        call(["mkdir", "-p", target_dir])
+
+        if open(dotfile, "r").exists and args.force:
+            logging.debug("Overwriting {}".format(target))
+            call(["rm", "-f", target])
+
+        if not open(target, "r").exists:
+            logging.debug("Symlinking {}->{}".format(target, source))
+            call(["ln", "-s", source, target])
+        else:
+            logging.debug("{} already exists")
 
 if args.install_packages or args.all:
-    print("Installing packages")
+    logging.info("Installing packages")
     install_targets = [package for package in packages if not is_installed(package)]
 
     if install_targets:
-        print("Found packages to install: {}".format(", ".join(install_targets)))
+        logging.debug("Found packages to install: {}".format(", ".join(install_targets)))
         call(["sudo", "pacman", "--noconfirm", "-S"] + install_targets)
 
 if args.install_aur_packages or args.all:
-    print("Installing AUR Packages")
+    logging.info("Installing AUR Packages")
     install_targets = [package for package in aur_packages if not is_installed(pacakge)]
 
     if install_targets:
-        print("Installing packages: {}".format(", ".join(install_targets)))
+        logging.debug("Installing packages: {}".format(", ".join(install_targets)))
         call(["sudo", "pacaur", "--noconfirm", "-S"] + install_targets)
 
-print("Done!")
+logging.info("Done!")
